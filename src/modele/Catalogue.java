b3d3e872a6e2fd81347fb8b15158aa52c83a5ae8
doclapin <sim_conejo@yahoo.fr>
2015-02-02 15:39:01 +0100

    Catalogue

--- src/modele/Catalogue.java
+++ src/modele/Catalogue.java
@@ -1,14 +1,52 @@
 package modele;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Catalogue {
 	private ArrayList<Produit> listeProduits = new ArrayList<Produit>();
-	
-	public Produit consulterProd(int num) {
-		return null;
-		
-	}
-	
 
+	public Produit consulterProd(int num) throws ClassNotFoundException {
+
+		if (initialiserCatalogue()) {
+
+			for (Produit produit : listeProduits) {
+				if (produit.getNumProd() == num)
+					return produit;
+
+			}
+
+		}
+		
+		return null;
+
+	}
+
+	public boolean initialiserCatalogue() throws ClassNotFoundException {
+
+		try {
+			Connection conn = ConnexionJDBC.connexion();
+
+			String req = "SELECT * FROM produit";
+			Statement stmt = conn.createStatement();
+			// execution de la requete
+			ResultSet res = stmt.executeQuery(req);
+
+			while (res.next()) {
+
+				listeProduits.add(new Produit(res.getInt("numProd"), res
+						.getString("designation"), res.getDouble("prix")));
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
 
 }
