44af59bfe9f1f318521a070b3e1d74ad5d56d6b9
doclapin <sim_conejo@yahoo.fr>
2015-02-02 17:01:15 +0100

    fini

--- src/modele/VenteSW.java
+++ src/modele/VenteSW.java
@@ -1,5 +1,6 @@
 package modele;
 
+import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
@@ -7,45 +8,85 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import javax.xml.rpc.ServiceException;
+
+import NET.webserviceX.www.Currency;
+import NET.webserviceX.www.CurrencyConvertorLocator;
+import NET.webserviceX.www.CurrencyConvertorSoap;
+
 public class VenteSW {
-	private ArrayList<Client> listeProduits = new ArrayList<Client>();
+	private ArrayList<Client> listeClients = new ArrayList<Client>();
 	private Catalogue catalogue;
-	
-//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//		
-//		Connection conn = ConnexionJDBC.connexion();
-//		String req = "SELECT * FROM produit p where p.numProd="+ 2;
-//		Statement stmt = conn.createStatement();
-//		// execu/on de la requete
-//		ResultSet res = stmt.executeQuery(req);
-//		res.next();
-//		System.out.println(res.getString("designation"));
-//	}
+
+	// public static void main(String[] args) throws ClassNotFoundException,
+	// SQLException {
+	//
+	// Connection conn = ConnexionJDBC.connexion();
+	// String req = "SELECT * FROM produit p where p.numProd="+ 2;
+	// Statement stmt = conn.createStatement();
+	// // execu/on de la requete
+	// ResultSet res = stmt.executeQuery(req);
+	// res.next();
+	// System.out.println(res.getString("designation"));
+	// }
 
 	// f1
-	public double consulterProd(int numeroProd, String monnaie) throws ClassNotFoundException {
+	public double consulterProd(int numeroProd, String monnaie)
+			throws ClassNotFoundException, ServiceException, RemoteException,
+			IllegalArgumentException {
+
 		double prixConverti;
+		String monnaieBase = "EUR";
 
 		Produit prod = catalogue.consulterProd(numeroProd);
 
-		prixConverti = 0;// appel au service web
+		CurrencyConvertorLocator service = new CurrencyConvertorLocator();
+
+		CurrencyConvertorSoap operation = service.getCurrencyConvertorSoap();
+
+		prixConverti = operation.conversionRate(
+				Currency.fromString(monnaieBase), Currency.fromString(monnaie))
+				* prod.getPrix(); // appel au service web
 
 		return prixConverti;
 	}
 
 	// f2
-	public Commande nouvelleCommande(Client cli) {
+	public Commande nouvelleCommande(String nomClient) throws ClassNotFoundException, SQLException {
 
-		// récupérer valeur séquence Commande
-		int seqCommande = 0;
+		Connection conn = ConnexionJDBC.connexion();
 
-		Commande cmd = new Commande(seqCommande, cli);
+		Client cli = null;
+		
+		try {
 
+			String req2 = "SELECT * FROM client WHERE nomClient = " + nomClient;
+			Statement stmt2 = conn.createStatement();
+			// execution de la requete
+			ResultSet res2 = stmt2.executeQuery(req2);
+
+			res2.next();
+
+			cli = new Client(nomClient, res2.getString("email"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// recuperer valeur sequence Commande
+		String req = "INSERT INTO commande(numCde, nomClient) VALUES (seq_commande.nextval, "
+				+ nomClient + " )";
+		Statement stmt = conn.createStatement();
+		// execu/on de la requete
+		ResultSet res = stmt.executeQuery(req);
+
+		Commande cmd = new Commande(res.getInt("numCde"), cli);
 		return cmd;
 	}
 
 	// f3
-	public DetailCde consulterCommande(int numCmd) throws SQLException, ClassNotFoundException {
+	public DetailCde consulterCommande(int numCmd) throws SQLException,
+			ClassNotFoundException {
 
 		DetailCde detail = new DetailCde();
 		Connection conn = ConnexionJDBC.connexion();
@@ -99,6 +140,36 @@ public class VenteSW {
 
 		return true;
 
+	}
+	
+	//f6
+	public Commande etatLivCommande(int numCde) throws ClassNotFoundException, SQLException {
+		
+		Connection conn = ConnexionJDBC.connexion();
+		
+		Commande cmd=null;
+
+		String req = "SELECT * FROM  commande c "
+				+ "WHERE c.numCde = " + numCde;
+		Statement stmt = conn.createStatement();
+		// execu/on de la requete
+		ResultSet res = stmt.executeQuery(req);
+		
+		String req2 = "SELECT * FROM client WHERE nomClient = " + res.getString("nomClient");
+		Statement stmt2 = conn.createStatement();
+		// execution de la requete
+		ResultSet res2 = stmt2.executeQuery(req2);
+
+		res2.next();
+
+		Client cli = new Client(res2.getString("nomClient"), res2.getString("email"));
+		cmd=new Commande(res.getInt("numCde"),cli);
+		cmd.setDateLiv(res.getDate("dateLiv"));
+		
+		return cmd;
+		
+		
+		
 	}
 
 }
